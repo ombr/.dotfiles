5bd2d318b3367effd17428c2d0bf548754fd1d3a
Luc Boissaye <luc@boissaye.fr>
2013-05-15 16:51:04 +0200

    Update.

--- .bin/pdflatex.sh
+++ .bin/pdflatex.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
-latex $1;
-dvips "$(basename $1 .tex).dvi" -o
-ps2pdf -sPAPERSIZE=a4 "$(basename $1 .tex).ps"
+if [ "$1" = "-w" ]i; then
+    echo supervisor -w ./ -x "$0 $2"
+else
+    echo $@
+    pwd
+    latex $1;
+    dvips "$(basename $1 .tex).dvi" -o
+    ps2pdf -sPAPERSIZE=a4 "$(basename $1 .tex).ps"
+fi
